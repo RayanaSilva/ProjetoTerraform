5e1da8d6743c80b73efd7e825d4522c3ff4578b6
RayanaSilva <117543221+RayanaSilva@users.noreply.github.com>
2024-10-20 14:57:40 -0300

    Update main.tf
    Havia comentado o código para a explicação do mesmo, mas refatorei para retirar os comentários e decidi documentar o código no arquivo README.md

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 terraform {
-  # Colocado um required providers específico caso a empresa utilize uma versão específica
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -30,32 +29,27 @@ variable "candidato" {
   default     = "rayana"
 }
 
-# Gera uma chave privada RSA de 2048 bits para ser usada como par de chaves SSH.
 resource "tls_private_key" "ec2_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
-# Cria um par de chaves na AWS com a chave pública gerada pelo recurso tls_private_key.
 resource "aws_key_pair" "ec2_key_pair" {
   key_name   = "${var.projeto}-${var.candidato}-key"
   public_key = tls_private_key.ec2_key.public_key_openssh
 }
 
-# Exibir a chave privada
 output "private_key" {
   description = "The private key generated for EC2"
   value       = tls_private_key.ec2_key.private_key_pem
   sensitive   = true  # Mantém a chave privada como sensível, não será registrada no Terraform state file
 }
 
-# Salvar a chave privada PEM em um arquivo local
 resource "local_file" "private_key_pem_file" {
   content  = tls_private_key.ec2_key.private_key_pem
   filename = "./private_key.pem"
 }
 
-# Cria uma VPC com o CIDR 10.0.0.0/16, habilitando suporte para DNS e hostnames dentro da rede.
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -66,7 +60,6 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-# Cria uma sub-rede dentro da VPC na zona de disponibilidade us-east-1a com o bloco CIDR 10.0.1.0/24.
 resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
@@ -77,7 +70,6 @@ resource "aws_subnet" "main_subnet" {
   }
 }
 
-# Anexa um gateway de internet à VPC para permitir a conectividade com a internet.
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -86,7 +78,6 @@ resource "aws_internet_gateway" "main_igw" {
   }
 }
 
-# Cria uma tabela de rotas para a VPC, permitindo rotas para a internet via o gateway de internet.
 resource "aws_route_table" "main_route_table" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -100,22 +91,17 @@ resource "aws_route_table" "main_route_table" {
   }
 }
 
-# Associa a tabela de rotas criada à sub-rede.
 resource "aws_route_table_association" "main_association" {
   subnet_id      = aws_subnet.main_subnet.id
   route_table_id = aws_route_table.main_route_table.id
 }
 
-# Cria um grupo de segurança que permite:
-# Entrada de tráfego SSH (porta 22) de qualquer lugar (IPv4 e IPv6).
-# Saída de todo o tráfego (todas as portas e protocolos) para qualquer destino.
 resource "aws_security_group" "main_sg" {
   name        = "${var.projeto}-${var.candidato}-sg"
   description = "Permitir SSH apenas de IP especifico e bloquear portas comuns de ataque"
   vpc_id      = aws_vpc.main_vpc.id
 
-  # Regras de entrada (permitir SSH apenas de um IP específico)
-  ingress {
+   ingress {
     description      = "Allow SSH from specific IP"
     from_port        = 22
     to_port          = 22
@@ -123,9 +109,6 @@ resource "aws_security_group" "main_sg" {
     cidr_blocks      = ["193.186.4.202/32"]  # Substituir pelo IP confiável
   }
 
-  # Não abrir portas comumente atacadas como 3389 (RDP), 23 (Telnet), etc.
-
-  # Regras de saída (permitir todo tráfego de saída)
   egress {
     description      = "Allow all outbound traffic"
     from_port        = 0
@@ -192,15 +175,11 @@ resource "aws_iam_role" "ec2_role" {
   EOF
 }
 
-# Este recurso anexa a política gerenciada da AWS chamada CloudWatchAgentServerPolicy à função IAM (role) atribuída à instância EC2. 
-# Isso permite que o CloudWatch Agent dentro da instância envie métricas e logs para o CloudWatch.
 resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-# O perfil da instância (instance profile) associa a função IAM à instância EC2, garantindo que a instância tenha as permissões 
-# necessárias para acessar serviços da AWS, como o CloudWatch.
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "${var.projeto}-${var.candidato}-ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
@@ -211,33 +190,21 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
 # Um script de inicialização (user_data) é executado para atualizar e atualizar os pacotes do # sistema assim que a instância for criada.
 resource "aws_instance" "debian_ec2" {
   depends_on = [aws_security_group.main_sg]
-  # Especifica o ID da imagem AMI para criar a instância EC2 (Debian 12 no caso).
   ami             = data.aws_ami.debian12.id
-  # Define o tipo da instância EC2 (t2.micro, uma das opções de uso gratuito da AWS).
   instance_type   = "t2.micro"
-  # Define a subnet onde a instância será criada.
   subnet_id       = aws_subnet.main_subnet.id
-  # Especifica o nome da chave SSH para acessar a instância.
   key_name        = aws_key_pair.ec2_key_pair.key_name
-  # Associa a instância a um ou mais grupos de segurança (security groups).
   security_groups = [aws_security_group.main_sg.id]
-  # Associa um perfil de instância IAM para que a instância EC2 tenha permissões, como enviar logs para o CloudWatch.
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
-  # Garante que a instância receberá um endereço IP público.
   associate_public_ip_address = true
 
-  # Configuração do disco raiz da instância.
   root_block_device {
-    # Tamanho do disco em GB.
     volume_size           = 20
-    # Tipo do disco (gp2 - SSD padrão da AWS).
     volume_type           = "gp2"
-    # Define que o disco será apagado quando a instância for terminada.
     delete_on_termination = true
   }
 
-  # Script de inicialização (user_data) que será executado quando a instância for iniciada.
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
@@ -246,10 +213,8 @@ resource "aws_instance" "debian_ec2" {
               systemctl start nginx
               systemctl enable nginx
 
-              # Instalação do CloudWatch Agent
               apt-get install -y amazon-cloudwatch-agent
 
-              # Configuração do CloudWatch Agent para enviar logs
               cat <<EOT >> /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
               {
                 "agent": {
@@ -278,11 +243,9 @@ resource "aws_instance" "debian_ec2" {
               }
               EOT
 
-              # Inicia o agente do CloudWatch para coletar e enviar logs.
               systemctl start amazon-cloudwatch-agent
               EOF
 
-  # Adiciona tags à instância, úteis para organização e identificação.
   tags = {
     Name = "${var.projeto}-${var.candidato}-ec2"
   }
